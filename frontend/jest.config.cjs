6425d668ed084a143971d0d4a7c430d542dc56f7
faye12022005 <duongnguyenminhkhang13@gmail.com>
2025-11-29 23:44:47 +0700

    Update Test Scenarios for Login again

--- frontend/jest.config.cjs
+++ frontend/jest.config.cjs
@@ -5,4 +5,9 @@ module.exports = {
   },
   moduleFileExtensions: ['js','jsx','ts','tsx','json'],
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+  // ignore Cypress specs and E2E-style files in src/tests
+  testPathIgnorePatterns: [
+    '<rootDir>/cypress/',
+    '<rootDir>/src/tests/.*\\.e2e\\.spec\\.(js|jsx|ts|tsx)$'
+  ],
 };
